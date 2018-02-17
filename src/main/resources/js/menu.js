f1a2767eeaacd3dcd4220120443baf8b19a34162
Harpal Singh <Harpal Singh@Harpal>
2018-02-17 11:28:31 +0530

    Initilization for project in a web app

---
+++ src/main/resources/js/menu.js
@@ -0,0 +1,7 @@
+function move() {
+  document.getElementById("old").classList.add("move-old");
+  document.getElementById("men").classList.add("move-men");
+  document.getElementById("kid").classList.add("move-kid");
+  document.getElementById("women").classList.add("move-women");
+
+}
