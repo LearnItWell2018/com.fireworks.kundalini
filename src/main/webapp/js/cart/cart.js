752534465195fd319787a16d4b0518988e21b9da
Harpal Singh <Harpal Singh@Harpal>
2018-02-25 00:56:04 +0530

    cart js done phase-1

--- src/main/webapp/js/cart/cart.js
+++ src/main/webapp/js/cart/cart.js
@@ -62,7 +62,7 @@ var init = function() {
  for (var i = 0; i < itemList.itemTypeList.length; i++) {
   var item = itemList.itemTypeList[i];
   console.log(item.item_name);
-  addTypeItem(document.getElementById("cart_items"), item.item_price);
+  addTypeItem(document.getElementById("cart_list"), item.item_price);
  }
 };
 
@@ -70,12 +70,19 @@ function addTypeItem(element, priceTag) {
  var nodeDivLevel1 = document.createElement("div");
  var nodeDivLevel21 = document.createElement("div");
  var nodeDivLevel22 = document.createElement("div");
+ var nodeDivLevel31 = document.createElement("div");
+ var nodeDivLevel13 = document.createElement("span");
+ var nodeDivLevel41 = document.createElement("div");
+ var nodeDivLevel51 = document.createElement("div");
  var removeCartButton = document.createElement("button");
+ var plusCartButton = document.createElement("button");
+ var minusCartButton = document.createElement("button");
  var nodePrice = document.createElement("i");
+ var quantityInput = document.createElement("input");
  var nodeCart = document.createElement("i");
  var image = document.createElement("img");
- var textnode = document.createTextNode("This is a paragraph.");
- var textPrice = document.createTextNode(priceTag);
+ var textDescriptionnode = document.createTextNode("This is a paragraph.");
+ var textPricenode = document.createTextNode(priceTag);
  /*var attDiv = document.createAttribute("class");
  attDiv.value = "rcorners";*/
  var attItemDiv = document.createAttribute("class");
@@ -120,10 +127,47 @@ function addTypeItem(element, priceTag) {
  var attItemImage = document.createAttribute("class");
  attItemImage.value = "image";
  nodeDivLevel22.setAttributeNode(attItemImage);
+ 
+ var attDescription = document.createAttribute("class");
+ attDescription.value = "description";
+ nodeDivLevel31.setAttributeNode(attDescription);
+ nodeDivLevel13.setAttributeNode(textDescriptionnode);
 
+ var attItemQuantity = document.createAttribute("class");
+ attItemQuantity.value = "quantity";
+ nodeDivLevel41.setAttributeNode(attItemQuantity);
+ 
+ var attPlusCartButton = document.createAttribute("class");
+ attPlusCartButton.value = "fa fa-plus";
+ plusCartButton.setAttributeNode(attPlusCartButton);
+ 
+ var attMinusCartButton = document.createAttribute("class");
+ attMinusCartButton.value = "fa fa-minus";
+ minusCartButton.setAttributeNode(attMinusCartButton);
+ 
+ var attquantityInput = document.createAttribute("type");
+ attquantityInput.value = "text";
+ attquantityInputValue = document.createAttribute("value");
+ attquantityInputValue.value = "1";
+ quantityInput.setAttributeNode(attquantityInput);
+ quantityInput.setAttributeNode(attquantityInputValue);
+ 
+ var attnodePrice = document.createAttribute("class");
+ attnodePrice.value = "fa fa-inr";
+ nodePrice.setAttributeNode(attnodePrice);
+ nodePrice.setAttributeNode(textPricenode);
+ 
+ nodeDivLevel41.appendChild(plusCartButton);//plus button in quantity
+ nodeDivLevel41.appendChild(quantityInput);//Item count in quantity
+ nodeDivLevel41.appendChild(minusCartButton);//minus button in quantity
+ nodeDivLevel51.appendChild(nodePrice);//inr icon added in div 5.
+ nodeDivLevel31.appendChild(nodeDivLevel13);//span in third div first of 3rd.
  nodeDivLevel22.appendChild(image);
- nodeDivLevel21.appendChild(removeCartButton);
- nodeDivLevel1.appendChild(nodeDivLevel21);
- nodeDivLevel1.appendChild(nodeDivLevel22);
- element.appendChild(nodeDivLevel1);
+ nodeDivLevel21.appendChild(removeCartButton);//Button in nodeDivLevel1
+ nodeDivLevel1.appendChild(nodeDivLevel21);//Div in Div with class item 
+ nodeDivLevel1.appendChild(nodeDivLevel22);//image set in main div
+ nodeDivLevel1.appendChild(nodeDivLevel31);//Description tag added in main div
+ nodeDivLevel1.appendChild(nodeDivLevel41);//Quantity node added in main div
+ nodeDivLevel1.appendChild(nodeDivLevel51);//div 5 added in main div
+ element.appendChild(nodeDivLevel1);//Main Div with class item 
 }
